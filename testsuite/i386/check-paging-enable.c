9b9c7babd4b1578ec6b71301c212f24a7ccca510
NalaGinrut <NalaGinrut@gmail.com>
2010-07-28 00:06:10 +0800

    UPDATE: testsuite/i386/check-paging-enable.c. still not complete

--- testsuite/i386/check-paging-enable.c
+++ testsuite/i386/check-paging-enable.c
@@ -18,6 +18,7 @@
 // This program could help you to check the paging_enabling order problem;
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define PG     	(0x1)
 #define PAE	((0x1)<<1)
@@ -39,129 +40,191 @@ typedef struct STATE_MACHINE
   int p_order;
   int GP;
   int next;
-}sm_t __attribute__((aligned(4)));
+  int stop;
+} sm_t __attribute__ ((aligned (4)));
 
 sm_t *sm_list[8];
-sm_t *path[10];// FIXME: use static list temporally,
-               // change it to dynamically list;
+sm_t *path[10];			// FIXME: use static list temporally,
+	       // change it to dynamically list;
 
 enum operation
 {
-  clr=0 ,set=1
-}op;
+  clr = 0, set = 1
+} op;
 
-sm_t current={ 0 ,0 ,0 };
+sm_t current = { 0, 0, 0, NO };
 
 // function declaration;
-int set_it(int what);
-int clr_it(int what);
-int begin();
-int handle_sel(int sel);
-int check_sm(int selector);
-int display_path();
-int display_op();
-int display_sel();
+int set_it (int what);
+int clr_it (int what);
+int begin ();
+int handle_sel (int sel);
+int check_op ();
+int check_sel ();
+int display_path ();
+int display_op ();
+int display_sel ();
 
 
-int main()
+int
+main ()
 {
 
-
+  begin ();
   return 0;
 }
 
-int set_it(int what)
+int
+set_it (int what)
 {
-  int ret=OK;
+  int ret = OK;
 
   current.p_order |= what;
-  
-  if( current.p_order > 7 ||
-      current.p_order == 5 )
-    ret=NO;
+
+  if (current.p_order > 7 || current.p_order == 5)
+    ret = NO;
 
   return OK;
 }
 
-int clr_it(int what)
+int
+clr_it (int what)
 {
-  int ret=OK;
-  
+  int ret = OK;
+
   current.p_order &= what;
-  
-  if( current.p_order > 7 ||
-      current.p_order == 5 )
-    ret=NO;
+
+  if (current.p_order > 7 || current.p_order == 5)
+    ret = NO;
 
   return OK;
 }
 
-int begin()
-{	
-  switch( getchar() )
+int
+begin ()
+{
+  while (current.GP == NO && current.stop == NO)
     {
-    case 's':
-      op=set;
-      printf("now you chose to 'set'!\n"); 
-      goto handle;
-    case 'c':
-      op=clr;
-      printf("now you chose to 'clr'!\n");
-      goto handle;
-    default:
-      display_op();
+      check_op ();
+      check_sel ();
     }
-  
+
+  display_path ();
+
   return 0;
 }
 
-int handle_sel(int sel)
-{	
-  int ret=OK;
-  switch(op)
+int
+check_op ()
+{
+  int ret = OK;
+  int ch = 0;
+
+  display_op ();
+
+  while (ch = getchar ())
+    {
+      if (ch == '\n')
+	continue;
+
+      switch (ch)
+	{
+	case 's':
+	  op = set;
+	  printf ("now you chose to 'set'!\n");
+	  goto handle;
+	case 'c':
+	  op = clr;
+	  printf ("now you chose to 'clr'!\n");
+	  goto handle;
+	case 'f':
+	  current.stop = OK;
+	  goto handle;
+	default:
+	  display_op ();
+	}
+    }
+handle:
+  return ret;
+}
+
+int
+handle_sel (int sel)
+{
+  int ret = OK;
+
+  switch (op)
     {
     case set:
-      ret=set_it(sel);
+      ret = set_it (sel);
       break;
     case clr:
-      ret=clr_it(sel);
+      ret = clr_it (sel);
       break;
     default:
-      printf("operation error! please check your program!\n");
-      exit(1);
+      printf ("operation error! please check your program!\n");
+      exit (1);
     }
   return ret;
 }
 
-int check_sm(int sel)
+int
+check_sel ()
 {
-  switch(sel)
-    {	
-    case 'p':
-      handle_sel(PG);
-      break;
-    case 'a':
-      handle_sel(PAE);
-      break;
-    case 'l':
-      handle_sel(LME);
-      break;
-    case 'q':
-      exit(0);
-    default:
-      greeting();
+  int ret = OK;
+  int ch = 0;
+
+  if (current.stop == OK)
+    return OK;
+
+  display_sel ();
+
+  while (ch = getchar ())
+    {
+      if (ch == '\n')
+	continue;
+
+      switch (ch)
+	{
+	case 'p':
+	  ret = handle_sel (PG);
+	  break;
+	case 'a':
+	  ret = handle_sel (PAE);
+	  break;
+	case 'l':
+	  ret = handle_sel (LME);
+	  break;
+	case 'q':
+	  exit (0);
+	default:
+	  display_sel ();
+	}
     }
-  
-  return 0;
+  return ret;
 }
 
-int display_path()
+int
+display_path ()
 {
+  printf ("path shows~~\n");
   return 0;
 }
 
-int display_op()
-{}
+int
+display_op ()
+{
+  printf ("Please chose a operator:\n"
+	  "[S]et\n" "[C]lear\n" "[F]inish\n" "input your chose:");
+  return 0;
+}
 
-int display_sel()
-{}
+int
+display_sel ()
+{
+  printf ("Please chose which mode should be set:\n"
+	  "[P]rotected Mode\n"
+	  "Physical [A]ddress Extended\n" "[L]ME\n" "input your chose:");
+  return 0;
+
+}
