9df5db23be010064ec880559744b4e6f29a16c84
Nala Ginrut <nalaginrut@gmail.com>
2018-02-11 16:32:58 +0800

    fixed MBR signature

--- scripts/sign.scm
+++ scripts/sign.scm
@@ -1,5 +1,5 @@
-;;  Copyright (C) 2012, 2016
-;;	"Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
+;;  Copyright (C) 2012,2016,2018
+;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
 ;;  This program is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
 ;;  the Free Software Foundation, either version 3 of the License, or
@@ -13,18 +13,19 @@
 ;;  You should have received a copy of the GNU General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define main
-  (lambda (args)
-    (and (< (length args) 2) (error "usage: ~a filename ~%" (car args)))
-    (let* ((filename (cadr args))
-	   (fp (open-file filename "r+"))) ;; use "r+", or we can't do 'seek trick'.
-      (unless (<= (stat:size (stat filename)) 510) 
-	(format #t "File: ~s size is larger than 510B!~%" filename) (primitive-exit))
-      (copy-file filename (string-append filename ".bak")) ;; A backup for debug
-      (seek fp 510 SEEK_SET) ;; pad zero automatically
-      (format fp "~a" "\x55\xAA") (close fp)
-      (rename-file filename (string-append filename ".out"))
-      (display "Seems the MBR has been generated successfully!!!\n"))))
+(use-modules (ice-9 match))
 
-
-
+(define (main args)
+  (match args
+    ((_ filename)
+     (let ((fp (open-file filename "r+b")))
+       (when (> (stat:size (stat filename)) 510)
+         (format #t "File: ~s size is larger than 510B!~%" filename)
+         (exit -1))
+       (copy-file filename (string-append filename ".bak")) ;; A backup for debug
+       (seek fp 510 SEEK_SET) ;; pad zero automatically
+       (format fp "\x55\xAA") (close fp)
+       (rename-file filename (string-append filename ".out"))
+       (display "Seems the MBR has been generated successfully!!!\n")))
+    (else
+     (format #t "usage: ~a filename ~%" (car args)))))
